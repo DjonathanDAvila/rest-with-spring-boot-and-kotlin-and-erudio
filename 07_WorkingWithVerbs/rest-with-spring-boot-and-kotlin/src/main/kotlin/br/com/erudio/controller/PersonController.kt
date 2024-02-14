7b20400691d8c3413377b50a251fdf9ccb230571
Djonathan D'Avila <123762143+DjonathanDAvila@users.noreply.github.com>
2024-02-13 21:22:18 -0300

    Adicionado os métodos POST/PUT/DELETE

--- 07_WorkingWithVerbs/rest-with-spring-boot-and-kotlin/src/main/kotlin/br/com/erudio/controller/PersonController.kt
+++ 07_WorkingWithVerbs/rest-with-spring-boot-and-kotlin/src/main/kotlin/br/com/erudio/controller/PersonController.kt
@@ -4,10 +4,7 @@ import br.com.erudio.model.Person
 import br.com.erudio.services.PersonService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.concurrent.atomic.AtomicLong
 
 @RestController
@@ -18,14 +15,47 @@ class PersonController {
 
     @Autowired // Quem vai fazer a instanciação é o Spring
     private lateinit var service: PersonService
-
     /*Substitui ->
       var service: PersonService = PersonService()*/
+
+    @RequestMapping(method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun findAll(): List<Person> {
+        return service.findAll()
+    }
+
     @RequestMapping(
         value = ["/{id}"], method = [RequestMethod.GET],
         produces = [MediaType.APPLICATION_JSON_VALUE]
     )
-    fun sum(@PathVariable(value = "id") id: Long): Person {
+    fun findById(@PathVariable(value = "id") id: Long): Person {
         return service.findById(id)
     }
+
+    @RequestMapping(
+        method = [RequestMethod.POST],
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun create(@RequestBody person: Person): Person {
+        return service.create(person)
+    }
+
+    @RequestMapping(
+        method = [RequestMethod.PUT],
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun update(@RequestBody person: Person): Person {
+        return service.update(person)
+    }
+
+    @RequestMapping(
+        value = ["/{id}"], method = [RequestMethod.DELETE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun delete(@PathVariable(value = "id") id: Long) {
+        service.delete(id)
+    }
+
+
 }
--- 07_WorkingWithVerbs/rest-with-spring-boot-and-kotlin/src/main/kotlin/br/com/erudio/services/PersonService.kt
+++ 07_WorkingWithVerbs/rest-with-spring-boot-and-kotlin/src/main/kotlin/br/com/erudio/services/PersonService.kt
@@ -9,6 +9,18 @@ class PersonService {
     private val counter: AtomicLong = AtomicLong()
     private val logger = java.util.logging.Logger.getLogger(PersonService::class.java.name)
 
+    fun findAll(): List<Person> {
+        logger.info("Finding all people!")
+
+        val persons: MutableList<Person> = ArrayList()
+
+        for (i in 0..7) {
+            val person = mockPerson(i)
+            persons.add(person)
+        }
+        return persons
+    }
+
     fun findById(id: Long): Person {
         logger.info("Finding one person!")
 
@@ -18,6 +30,24 @@ class PersonService {
         person.lasttName = "D'Avila"
         person.addrss = "Indaial - Santa Catarina - Brasil"
         person.gender = "Male"
+
+        return person
+    }
+
+    fun create(person: Person) = person
+
+    fun update(person: Person) = person
+
+    fun delete(id: Long) {}
+
+    private fun mockPerson(i: Int): Person {
+        val person = Person()
+
+        person.id = counter.incrementAndGet()
+        person.firstName = "Person name $i"
+        person.lasttName = "Las name $i"
+        person.addrss = "Address $i"
+        person.gender = "Gender $i"
 
         return person
     }
