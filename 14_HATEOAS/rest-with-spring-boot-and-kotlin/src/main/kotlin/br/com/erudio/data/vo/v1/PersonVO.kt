3bf932435f534fc71e314b100fb3922eba1ccdbb
Djonathan D'Avila <123762143+DjonathanDAvila@users.noreply.github.com>
2024-03-09 16:37:26 -0300

    First commit Seção 9

---
+++ 14_HATEOAS/rest-with-spring-boot-and-kotlin/src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
@@ -0,0 +1,13 @@
+package br.com.erudio.data.vo.v1
+
+import com.github.dozermapper.core.Mapping
+import org.springframework.hateoas.RepresentationModel
+
+data class PersonVO(
+    @Mapping("id")
+    var idPerson: Long = 0,
+    var firstName: String = "",
+    var lastName: String = "",
+    var address: String = "",
+    var gender: String = "",
+) : RepresentationModel<PersonVO>()
